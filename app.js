a32466cf9e618a75979893a5832e5e63922347ce
Rajat Kumar Pandey <122084208+raj2410p@users.noreply.github.com>
2025-06-22 19:11:56 +0530

    inital commit

--- app.js
+++ app.js
@@ -54,6 +54,7 @@ app.use(express.static(path.join(__dirname, 'client', 'dist')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 });
-app.listen(process.env.PORT || 3001, () => {
+const PORT = process.env.PORT || 3001;
+app.listen(process.env.PORT || PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
